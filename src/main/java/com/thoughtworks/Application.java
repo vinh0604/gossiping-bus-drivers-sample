8ddeb92eaf9e072a1e9df8178701d2e2d0f1a5b8
vinhbachsy <bsdvinhuit@gmail.com>
2019-06-16 21:06:22 +0800

    Initial commit

---
+++ src/main/java/com/thoughtworks/Application.java
@@ -0,0 +1,10 @@
+package com.thoughtworks;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class Application {
+    public static void run(InputStream inputStream, PrintStream out) {
+
+    }
+}
---
+++ src/test/java/com/thoughtworks/ApplicationTest.java
@@ -0,0 +1,28 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ApplicationTest {
+    @Mock
+    private PrintStream out;
+
+    @Test
+    public void shouldReturnTheNumberOfStopsRequired_whenAllBusDriversCanMeetEachOther() {
+        Application.run(getClass().getResourceAsStream("/input-1.txt"), out);
+        verify(out).println("5");
+    }
+
+    @Test
+    public void shouldReturnNever_whenSomeBusDriversCanNotMeet() {
+        Application.run(getClass().getResourceAsStream("/input-2.txt"), out);
+        verify(out).println("never");
+    }
+}
